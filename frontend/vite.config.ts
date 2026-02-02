1be88c76efd6103892083d50d6e66747473fecf4
gravitaciy <gravitaciy2@gmail.com>
2026-02-02 23:44:15 +0700

    setup vue

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,17 +2,23 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import ui from '@nuxt/ui/vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    ui({ router: false }),
     vueDevTools(),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
+  },
+  server: {
+    host: '127.0.0.1', // Принудительно используем 127.0.0.1 вместо localhost
+    port: 5173, // Можно указать конкретный порт, если нужно
   },
 })
